a9331e078cd98b09370b9f801c7d3f4befbe136c
Yeji Han <hany7@hawaii.edu>
2020-10-14 19:36:32 -1000

    3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Header } from 'semantic-ui-react';
 
 class Stussy extends React.Component {
 
